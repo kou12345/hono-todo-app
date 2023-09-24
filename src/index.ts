870023042879ef1c1c07eabb5739659e2c22b446
kou12345 <hi.0412.ko.2002@gmail.com>
2023-09-24 19:41:58 +0900

    fix Bindings

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import { Hono } from 'hono';
 import { ExecutionContext } from 'hono/dist/types/context';
 import { users } from './users/api';
 
-export interface Bindings {
+export type Bindings = {
 	DB_URL: string;
 	ctx: ExecutionContext;
-}
+};
 
-// @ts-ignore
 const app = new Hono<{ Bindings: Bindings }>();
 app.route('/api/users', users);
 
